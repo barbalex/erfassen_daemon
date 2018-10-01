cf3c796a566aed0a1fd5dc0b92b2922ef00bf845
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-01 18:04:30 +0200

    rename

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const dbUrl = `http://${user}:${pass}@127.0.0.1:5984`
 const nano = require('nano')(dbUrl)
 const filter = require('lodash/filter')
 
-const listenToChangesIn_usersDb = require('./modules/listenToChangesIn_usersDb')
-const listenToChangesInUsersDbs = require('./modules/listenToChangesInUsersDbs')
-const listenToChangesInMessageDb = require('./modules/listenToChangesInMessageDb')
-const listenToDbChanges = require('./modules/listenToDbChanges')
+const listenToChangesIn_usersDb = require('./src/listenToChangesIn_usersDb')
+const listenToChangesInUsersDbs = require('./src/listenToChangesInUsersDbs')
+const listenToChangesInMessageDb = require('./src/listenToChangesInMessageDb')
+const listenToDbChanges = require('./src/listenToDbChanges')
 
 consr run = async() => {
   let dbs
