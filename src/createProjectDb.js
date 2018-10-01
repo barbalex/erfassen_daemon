014b2a4940b9aedf1379a28d337cdc4928ee63ce
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-01 22:31:57 +0200

    optimize

--- src/createProjectDb.js
+++ src/createProjectDb.js
@@ -5,43 +5,38 @@
 
 'use strict'
 
-const contains = require('lodash/contains')
 const createSecurityDoc = require('./createSecurityDoc')
 
 module.exports = (nano, projectDbName) => {
-  var securityDoc
-
   // create new projectDb's if it doesn't exist yet
   // get list of DB's in couch
-  nano.db.list(function(error, dbNames) {
-    if (error) {
-      return console.log("error getting list of db's: ", error)
-    }
-    // create new projectDb if it does not exist yet
-    if (!contains(dbNames, projectDbName)) {
-      nano.db.create(projectDbName, function(error) {
-        if (error) {
-          return console.log(
-            'error creating new db ' + projectDbName + ':',
-            error,
-          )
-        }
+  let dbNames
+  try {
+    await dbNames = nano.db.list()
+  } catch (error) {
+    return console.log("error getting list of db's: ", error)
+  }
+  if (dbNames.includes(projectDbName)) return
+  // create new projectDb if it does not exist yet
+  try {
+    await nano.db.create(projectDbName)
+  } catch (error) {
+    return console.log('error creating new db ' + projectDbName + ':', error)
+  }
+  console.log('created new db: ', projectDbName)
 
-        console.log('created new db: ', projectDbName)
-
-        // set up permissions for this role
-        securityDoc = createSecurityDoc(null, projectDbName, couchPassfile.user)
-        nano
-          .use(projectDbName)
-          .insert(securityDoc, '_security', function(error) {
-            if (error) {
-              return console.log(
-                'error setting _security in new db ' + projectDbName + ': ',
-                error,
-              )
-            }
-          })
-      })
-    }
-  })
+    // set up permissions for this role
+    const securityDoc = createSecurityDoc(
+      null,
+      projectDbName,
+      couchPassfile.user,
+    )
+    nano.use(projectDbName).insert(securityDoc, '_security', function(error) {
+      if (error) {
+        return console.log(
+          'error setting _security in new db ' + projectDbName + ': ',
+          error,
+        )
+      }
+    })
 }
