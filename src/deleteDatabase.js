067ad3d29fd85acf25e99c22ac697eca30252570
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-03 15:34:45 +0200

    try to get eslint and prettier to work

--- src/deleteDatabase.js
+++ src/deleteDatabase.js
@@ -1,4 +1,4 @@
-'use strict'
+
 const uniq = require('lodash/uniq')
 const flatten = require('lodash/flatten')
 
@@ -10,37 +10,33 @@ module.exports = async (nano, dbName) => {
   } catch (error) {
     return console.log('error getting list of dbs: ', error)
   }
-  if (!dbNames.includes(dbName) return
-    // check if a user uses this db
-    nano.use('_users').list(function(error, body) {
-      if (error) {
-        return console.log(
-          'error getting list of users from _users db: ',
-          error,
-        )
-      }
-      var users = body.rows
+  if (!dbNames.includes(dbName)) return
+  // check if a user uses this db
+  let _usersBody
+  try {
+    _usersBody = nano.use('_users').list()
+  } catch (error) {
+    return console.log('error getting list of users from _users db:', error)
+  }
+  const users = _usersBody.rows
 
-      const usersRoles = uniq(flatten(users.map(user => user.roles)))
+  const usersRoles = uniq(flatten(users.map(user => user.roles)))
 
-      if (usersRoles.includes(dbname)) {
-        // another user uses this db
-        // DONT remove it
-        console.log(`db ${dbName} not deleted because used by other user`)
-      } else {
-        // stop listening to changes
-        if (GLOBAL[dbName]) {
-          GLOBAL[dbName].stop()
-          console.log(
-            `deleteDatabase: stopped listening to feed of ${dbName}`,
-          )
-        }
-        nano.db.destroy(dbName, function(err) {
-          if (err) {
-            return console.log(`error deleting database ${dbName}:`, err)
-          }
-          console.log(`deleteDatabase: deleted database ${dbName}`)
-        })
+  if (usersRoles.includes(dbname)) {
+    // another user uses this db
+    // DONT remove it
+    console.log(`db ${dbName} not deleted because used by other user`)
+  } else {
+    // stop listening to changes
+    if (GLOBAL[dbName]) {
+      GLOBAL[dbName].stop()
+      console.log(`deleteDatabase: stopped listening to feed of ${dbName}`)
+    }
+    nano.db.destroy(dbName, function(err) {
+      if (err) {
+        return console.log(`error deleting database ${dbName}:`, err)
       }
+      console.log(`deleteDatabase: deleted database ${dbName}`)
     })
+  }
 }
