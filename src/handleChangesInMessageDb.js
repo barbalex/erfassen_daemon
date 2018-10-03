3ecef2f995351162cb3b66ca5272596c12b8caef
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-03 15:09:31 +0200

    get eslint to work

--- src/handleChangesInMessageDb.js
+++ src/handleChangesInMessageDb.js
@@ -16,17 +16,17 @@ module.exports = async change => {
   const mDb = nano.use('erfassen_messages')
 
   if (doc.type === 'projectAdd') (
-      createProjectDb(nano, doc.projectName)
-      // now delete the message
-      mDb.destroy(doc, doc._rev, function(error) {
-        if (error) {
-          return console.log(
-            'error removing doc after ordering to create project',
-            error,
-          )
-        }
+    createProjectDb(nano, doc.projectName)
+    // now delete the message
+    mDb.destroy(doc, doc._rev, function(error) {
+      if (error) {
+        return console.log(
+          'error removing doc after ordering to create project',
+          error,
+        )
+      }
 
-        console.log('removed doc after ordering to create project', doc)
-      })
+      console.log('removed doc after ordering to create project', doc)
+    })
   )
 }
