41583ba0418bad9bba599a3f1f6925d84c93c2fd
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-03 14:57:39 +0200

    optimize

--- src/handleChangesIn_usersDb.js
+++ src/handleChangesIn_usersDb.js
@@ -74,122 +74,116 @@ const onCreatedUserDb = async (userName, userDbName, userDoc) => {
 
 module.exports = async change => {
   // check the revs
-  nano
-    .use('_users')
-    .get(change.id, { revs: true, open_revs: 'all' }, function(error, body) {
-      if (error) {
-        return console.log(
-          'handleChangesIn_usersDb: error getting revs of doc: ',
-          error,
+  let revisionsBody
+  try {
+    revisionsBody = await nano
+      .use('_users')
+      .get(change.id, { revs: true, open_revs: 'all' })
+  } catch (error) {
+    return console.log(
+      'handleChangesIn_usersDb: error getting revs of doc: ',
+      error,
+    )
+  }
+  const revisions = body[0].ok._revisions
+  const revOfOldDoc = revisions.start - 1 + '-' + revisions.ids[1]
+  const messageDb = nano.use('erfassen_messages')
+
+  // a new user was created, an existing changed or deleted
+  if (change.deleted) {
+    // user was deleted > no doc in change
+    // get last doc version before deleted to know the user.name and user.roles
+    let doc
+    try {
+      doc = await nano.use('_users').get(change.id, { rev: revOfOldDoc })
+    } catch (error) {
+      if (error.statusCode === 404) {
+        // could not get previous version - this is expected if user was created new before
+        return true
+      }
+      return console.log(
+        'handleChangesIn_usersDb: error getting userDoc version before deleted:',
+        error,
+      )
+    }
+    if (doc) {
+      // a user was deleted
+      const userName = doc.name
+      const userProjects = doc.roles
+      const userDbName = getUserDbName(userName)
+
+      // remove the role from all the users docs
+      // remove projects and their db's that only had this user
+      removeUsersProjectDbs(nano, userName, userProjects)
+
+      // delete this user's database
+      deleteDatabase(nano, userDbName)
+
+      // stop listening to changes to userDb
+      if (GLOBAL[userDbName]) {
+        GLOBAL[userDbName].stop()
+        console.log(
+          `handleChangesIn_usersDb: stopped listening to feed of ${userDbName}`,
         )
       }
 
-      var revisions = body[0].ok._revisions,
-        revOfOldDoc = revisions.start - 1 + '-' + revisions.ids[1],
-        userDoc,
-        userDbName,
-        userName,
-        userProjects,
-        messageDb
+      // remove user from members of message db
+      let securityDoc
+      try {
+        securityDoc = await messageDb.get('_security')
+      } catch (error) {
+        console.log(
+          'handleChangesIn_usersDb: error getting _security of message db:',
+          error,
+        )
+      }
+      securityDoc.members.names = without(securityDoc.members.names, userName)
+    }
+  } else {
+    // PROBLEM: userDb gets created when userDb was removed,
+    // because the roles are then removed from _users db
+    // solution: handleDbChanges passes GLOBAL.deleteUserDb
+    if (GLOBAL.deleteUserDb) {
+      return delete GLOBAL.deleteUserDb
+    }
 
-      messageDb = nano.use('erfassen_messages')
-
-      // a new user was created, an existing changed or deleted
-      if (change.deleted) {
-        // user was deleted > no doc in change
-        // get last doc version before deleted to know the user.name and user.roles
-        nano
-          .use('_users')
-          .get(change.id, { rev: revOfOldDoc }, function(error, doc) {
-            if (error) {
-              if (error.statusCode === 404) {
-                // could not get previous version - this is expected if user was created new before
-                return true
-              }
-              return console.log(
-                'handleChangesIn_usersDb: error getting userDoc version before deleted: ',
-                error,
-              )
-            }
-            if (doc) {
-              // a user was deleted
-              userName = doc.name
-              userProjects = doc.roles
-              userDbName = getUserDbName(doc.name)
-
-              // remove the role from all the users docs
-              // remove projects and their db's that only had this user
-              removeUsersProjectDbs(nano, userName, userProjects)
-
-              // delete this user's database
-              deleteDatabase(nano, userDbName)
-
-              // stop listening to changes to userDb
-              if (GLOBAL[userDbName]) {
-                GLOBAL[userDbName].stop()
-                console.log(
-                  `handleChangesIn_usersDb: stopped listening to feed of ${userDbName}`
-                )
-              }
-
-              // remove user from members of message db
-              let securityDoc
-              try {
-                securityDoc = await messageDb.get('_security')
-              } catch (error) {
-                console.log(
-                  'handleChangesIn_usersDb: error getting _security of message db:',
-                  error,
-                )
-              }
-              securityDoc.members.names = without(securityDoc.members.names, userName)
-            }
-          })
-      } else {
-        // PROBLEM: userDb gets created when userDb was removed,
-        // because the roles are then removed from _users db
-        // solution: handleDbChanges passes GLOBAL.deleteUserDb
-        if (GLOBAL.deleteUserDb) {
-          return delete GLOBAL.deleteUserDb
-        }
-
-        userDoc = change.doc
-        userName = userDoc.name
-        userDbName = getUserDbName(userName)
-        // get list of all databases
-        let dbNames
-        try {
-          dbNames = await nano.db.list()
-        } catch (error) {
-          return console.log(
-            'handleChangesIn_usersDb: error getting list of dbs:', error,
-          )
-        }
-        if (!dbNames.includes(userDbName)) {
-            // this user has no uderDb yet
-            // a new user was created
-            // create a new user db if it does not exist yet
-            nano.db.create(userDbName, function(error) {
-              if (error) {
-                if (error.statusCode === 412) {
-                  // db exists already
-                  // go on incase roles have not been created yet
-                } else {
-                  return console.log(
-                    'handleChangesIn_usersDb: error creating new user database ' +
-                      userDbName +
-                      ': ',
-                    error,
-                  )
-                }
-              }
-              onCreatedUserDb(userName, userDbName, userDoc)
-            })
+    const userDoc = change.doc
+    const userName = userDoc.name
+    const userDbName = getUserDbName(userDoc.name)
+    // get list of all databases
+    let dbNames
+    try {
+      dbNames = await nano.db.list()
+    } catch (error) {
+      return console.log(
+        'handleChangesIn_usersDb: error getting list of dbs:',
+        error,
+      )
+    }
+    if (!dbNames.includes(userDbName)) {
+      // this user has no uderDb yet
+      // a new user was created
+      // create a new user db if it does not exist yet
+      nano.db.create(userDbName, function(error) {
+        if (error) {
+          if (error.statusCode === 412) {
+            // db exists already
+            // go on incase roles have not been created yet
           } else {
-            // problem: somehow the userdb is created directly by the app
-            // dont know why, it should be impossible
-            onCreatedUserDb(userName, userDbName, userDoc)
+            return console.log(
+              'handleChangesIn_usersDb: error creating new user database ' +
+                userDbName +
+                ': ',
+              error,
+            )
           }
         }
-    })
+        onCreatedUserDb(userName, userDbName, userDoc)
+      })
+    } else {
+      // problem: somehow the userdb is created directly by the app
+      // dont know why, it should be impossible
+      onCreatedUserDb(userName, userDbName, userDoc)
+    }
+  }
 }
