a86f8ebc9f3266bed7d081b253b32ce9b6e4bc8c
alex.barbalex@gmail.com <URj8XXYKl1A9>
2018-10-15 13:01:30 +0200

    test db name

--- src/getProjectDbName.js
+++ src/getProjectDbName.js
@@ -1,8 +1,14 @@
-module.exports = ({ userName, projectName }) =>
-  'project_' +
-  userName
-    .toLowerCase()
-    .replace('@', '_at_')
-    .replace('.', '_p_') +
-  '_' +
-  projectName
+module.exports = ({ userName, projectName }) => {
+  const name =
+    'project_' +
+    userName
+      .toLowerCase()
+      .replace('@', '_at_')
+      .replace('.', '_p_') +
+    '_' +
+    projectName
+  if (!/^[a-z][a-z0-9_$()+/-]*$/.test(name)) {
+    return console.log('Name is not valid db name')
+  }
+  return name
+}
