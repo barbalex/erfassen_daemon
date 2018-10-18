6095da93d26581fe8523ae49560de3238aae6c26
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-18 11:34:17 +0200

    catch error

--- src/listenToChangesInMessageDb.js
+++ src/listenToChangesInMessageDb.js
@@ -11,6 +11,15 @@ module.exports = () => {
     include_docs: true,
   })
   feed.on('change', handleChangesInMessagesDb)
+  feed.on('error', error => {
+    if (error.message.includes('Database deleted after change')) {
+      // I have deleted message db in dev mode
+      console.log('messages db was deleted while being followed')
+      return global.messages.stop()
+    }
+    console.log('error following messages db', { msg: error.message })
+    global.messages.stop()
+  })
   feed.follow()
   // make sure that follow is only called once
   global.messages = feed
