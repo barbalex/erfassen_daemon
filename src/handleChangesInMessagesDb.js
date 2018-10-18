e67f21430a86e20017c98cfef67fac2a6a3950a2
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-18 11:36:07 +0200

    delete message after creating project

--- src/handleChangesInMessagesDb.js
+++ src/handleChangesInMessagesDb.js
@@ -26,9 +26,13 @@ module.exports = async change => {
       projectDbName,
     })*/
     await createProjectDb(projectDbName)
-    nano.use(projectDbName).insert({ creatorName, projectName, type })
+    nano
+      .use(projectDbName)
+      .insert({ _id: 'projectDef', creatorName, projectName, type })
     console.log(
       `handleChangesInMessagesDb: created new project db '${projectDbName}'`,
     )
+    nano.use('messages').destroy(doc._id, doc._rev)
+    console.log(`handleChangesInMessagesDb: removed message '${doc.type}'`)
   }
 }
