aa4b45b770a129cd217788728c2ab5c4d9f9e479
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-18 11:34:28 +0200

    remove unused

--- src/deleteDatabase.js
+++ src/deleteDatabase.js
@@ -1,6 +1,3 @@
-const uniq = require('lodash/uniq')
-const flatten = require('lodash/flatten')
-
 module.exports = async (nano, dbName) => {
   // first check if this db still exists
   let dbNames
@@ -10,21 +7,7 @@ module.exports = async (nano, dbName) => {
     return console.log('error getting list of dbs: ', error)
   }
   if (!dbNames.includes(dbName)) return
-  // check if a user uses this db
-  let _usersBody
-  try {
-    _usersBody = await nano.use('_users').list()
-  } catch (error) {
-    return console.log('error getting list of users from _users db:', error)
-  }
-  const users = _usersBody.rows
-  const usersRoles = uniq(flatten(users.map(user => user.roles)))
 
-  if (usersRoles.includes(dbName)) {
-    // another user uses this db
-    // DONT remove it
-    return console.log(`db ${dbName} not deleted because used by other user`)
-  }
   // stop listening to changes
   if (global[dbName]) {
     global[dbName].stop()
